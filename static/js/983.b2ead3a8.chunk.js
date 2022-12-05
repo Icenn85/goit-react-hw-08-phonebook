0e66f2f508eab757e1f4ecd706482b0f87b379e4
Icenn85 <99094185+Icenn85@users.noreply.github.com>
2022-12-05 20:15:32 +0000

    Deploying to gh-pages from @ Icenn85/goit-react-hw-08-phonebook@9d1771d407fcbadf41176f024a5722a2a04f316b 🚀

---
+++ static/js/983.b2ead3a8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{3983:function(e,t,n){n.r(t);n(2791);var o=n(184),c={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center",color:"#210B61"}};t.default=function(){return(0,o.jsx)("div",{style:c.container,children:(0,o.jsx)("h1",{style:c.title,children:"Welcome to our phonebook"})})}}}]);
+//# sourceMappingURL=983.b2ead3a8.chunk.js.map
